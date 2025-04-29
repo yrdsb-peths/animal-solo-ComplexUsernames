425b5c790e8784a64e706edf69f05b84a27dea4b
ComplexUsernames <349462549@gapps.yrdsb.ca>
2025-04-29 08:40:40 -0400

    Add elephant into world

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,16 @@
 import greenfoot.*;
 
+/**
+ * The World that the main character is in.
+ * 
+ * @author Li
+ * @version April 2025
+ */
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 300);
     }
 }
